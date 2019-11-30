925bde464890d6586b3b789339214c33066489f2
whdms705 <whdms705@nate.com>
2019-11-30 11:23:15 +0900

    add ch02 변수

---
+++ src/ch02/TypeIntro.kt
@@ -0,0 +1,16 @@
+package ch02
+
+// 컴파일 시점 상수 선언하기
+const val MAX_EXPERIENCE: Int = 5000;
+
+fun main(args: Array<String>) {
+    var experiencePoints = 5;
+    experiencePoints += 5;
+    println(experiencePoints);
+
+    // 읽기전용 변수 선언
+    val playerName = "에스트라곤";
+    println(playerName)
+
+    println(MAX_EXPERIENCE)
+}
