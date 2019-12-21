d57a965c99db17031820aedc9cc623599834863c
whdms705 <whdms705@nate.com>
2019-12-22 02:38:38 +0900

    Add ch11 Map

--- src/ch10/Tavern.kt
+++ src/ch10/Tavern.kt
@@ -16,4 +16,16 @@ fun main(args: Array<String>) {
     println(patronList.first())
 
     println(patronList.last())
+
+
+    // 존재 하지않는 인덱스 사용시
+    //println(patronList[4])
+
+    // getOrElse
+    println(patronList.getOrElse(4){"Unknown Patron"})
+
+
+    // getOrNull 테스트하기
+    val fifthPatron = patronList.getOrNull(4) ?: "Unknown Patron 2"
+    println(fifthPatron)
 }
---
+++ src/ch11/TavernMap.kt
@@ -0,0 +1,7 @@
+package ch11
+
+val partronGold = mapOf("Elo" to 10.5 ,"Mordoc" to 8.0 ,"Sophie" to 5.5 )
+
+fun main(args: Array<String>) {
+    println(partronGold)
+}
