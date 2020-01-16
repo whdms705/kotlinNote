a526644589faa0abd0571e6a3129257d3b863916
whdms705 <whdms705@nate.com>
2020-01-16 23:53:22 +0900

    Update ch16 추상클래스

--- src/ch16/Creature.kt
+++ src/ch16/Creature.kt
@@ -13,3 +13,25 @@ interface Fightable {
 
     fun attack(opponent: Fightable):Int
 }
+
+// 추상클래스 정의
+abstract class Monster(val name : String
+                       , val description : String
+                       , override var healthPoints: Int) : Fightable {
+
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = damageRoll
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
+}
+
+// Monster 추상클래스의 서브 클래스 생성
+class Goblin(name : String = "Goblin"
+            ,description: String = "추악하게 생긴 고블린"
+            ,healthPoints: Int = 30) : Monster(name, description, healthPoints)
+{
+    override val diceSides = 2
+    override val dicecount = 8
+
+}
